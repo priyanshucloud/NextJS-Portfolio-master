9b0f46c818a2ffc3513975fd1be8d721d4c42f56
priyanshucloud <admin@priyanshusharma.tech>
2024-07-27 23:03:33 +0530

    Update education.jsx

--- sections/education.jsx
+++ sections/education.jsx
@@ -59,7 +59,7 @@ const Education = () => {
                 className='hidden md:block bg-blue-400'
                 height={150}
                 src={education.image}
-                width={150}
+                width={104}
               />
               <div className='flex flex-col gap-2 p-3 md:p-1'>
                 <p className='text-xl md:text-2xl font-bold text-red-600'>
