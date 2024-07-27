700c2fb5c371be53b9284854a78ad3bda1e622db
priyanshucloud <admin@priyanshusharma.tech>
2024-07-27 21:10:17 +0530

    Update themeContext.js

--- context/themeContext.js
+++ context/themeContext.js
@@ -4,35 +4,26 @@ import React, { useState, createContext, useEffect } from "react";
 export const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState("dark");
+    const [theme, setTheme] = useState("light");
 
     // Toggle Theme
     const setThemeFun = () => {
-        if (theme === "dark") {
-            setTheme("light");
-            localStorage.setItem("myPortfolioProfileTheme", "light");
-        } else {
-            setTheme("dark");
-            localStorage.setItem("myPortfolioProfileTheme", "dark");
-        }
+        const newTheme = theme === "dark" ? "light" : "dark";
+        setTheme(newTheme);
+        localStorage.setItem("myPortfolioProfileTheme", newTheme);
     };
 
     // Get Theme Value From LocalStorage
     useEffect(() => {
-        const getTheme = localStorage.getItem("myPortfolioProfileTheme");
-        if (!getTheme) {
-            return
-        }
+        const getTheme = localStorage.getItem("myPortfolioProfileTheme") || "light";
         setTheme(getTheme);
     }, []);
 
     return (
         <ThemeContext.Provider value={{ theme, setThemeFun }}>
-            <div className={theme === "dark" ? "dark" : ""}>
+            <div className={theme}>
                 <div className='dark:text-white dark:bg-black'>{children}</div>
             </div>
         </ThemeContext.Provider>
     );
 };
-
-export default ThemeProvider;
