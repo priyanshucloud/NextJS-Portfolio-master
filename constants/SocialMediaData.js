f55d3ce23ce2a30e7b9ff18d19c94f7bbe93b98a
priyanshucloud <admin@priyanshusharma.tech>
2024-07-29 21:35:38 +0530

    Update SocialMediaData.js

--- constants/SocialMediaData.js
+++ constants/SocialMediaData.js
@@ -8,21 +8,21 @@ export const SocialMediaData = [
     icon: <AiOutlineInstagram />,
     color:
       "linear-gradient(135deg, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #FFDC80)",
-    link: "https://www.instagram.com/iam_shiv_726",
+    link: "https://www.instagram.com/its_13_shaan",
   },
   {
     icon: <IoClose />,
     color: "#0f0f0f",
-    link: "https://twitter.com/ShivrajGurjar15",
+    link: "https://twitter.com",
   },
   {
     icon: <FaLinkedinIn />,
     color: "#0072b1",
-    link: "https://www.linkedin.com/in/shivraj-dev",
+    link: "https://www.linkedin.com/in/",
   },
   {
     icon: <BsGithub />,
     color: "#171515",
-    link: "https://www.github.com/iamshiv007",
+    link: "https://www.github.com/priyanshucloud",
   },
 ];
