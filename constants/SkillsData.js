14e03567d32547913ed851f0db59a4c27965a406
priyanshucloud <admin@priyanshusharma.tech>
2024-07-30 22:26:16 +0530

    Update SkillsData.js
    change in skills

--- constants/SkillsData.js
+++ constants/SkillsData.js
@@ -5,67 +5,69 @@ import {
   SiVercel,
   SiNetlify,
   SiExpress,
-  SiRedux,
-  SiTailwindcss,
-  SiMui,
-  SiChakraui,
   SiFirebase,
   SiTypescript,
-  SiJquery
+  SiJquery,
+  SiMicrosoftazure,
+  SiNginx,
+  SiVirtualbox,
+  SiFuturelearn
+
 } from "react-icons/si";
 import { DiJavascript1, DiReact, DiMongodb, DiSass } from "react-icons/di";
 import { BsGit, BsGithub } from "react-icons/bs";
-import { FaBootstrap, FaCss3Alt, FaAngular, FaPhp, FaShopify, FaJava } from "react-icons/fa";
-import { TbBrandReactNative } from "react-icons/tb";
+import { FaBootstrap, FaCss3Alt, FaAngular, FaPhp, FaShopify, FaJava, FaAws, FaNetworkWired, FaUbuntu, FaServer, FaWordpress} from "react-icons/fa";
+import { TbBrandReactNative, TbCloudComputing } from "react-icons/tb";
 import { GrMysql } from "react-icons/gr";
 
 export const TechStackData = [
   {
     Advance: [
       {
-        name: "ReactJS",
-        icon: <DiReact className="md:text-4xl text-2xl" color="#53c1de" />,
+        name: "Azure",
+        icon: <SiMicrosoftazure className="md:text-4xl text-2xl" color="#007FFF" />,
       },
       {
-        name: "JavaScript",
-        icon: <DiJavascript1 className="md:text-4xl text-2xl" color="#ffd600" />,
-      },
-      {
-        name: "Redux-toolkit",
-        icon: <SiRedux className="md:text-4xl text-2xl" color="#7e57c2" />,
-      },
-      {
-        name: "HTML5",
-        icon: <AiFillHtml5 className="md:text-4xl text-2xl" color="#fa6700" />,
-      },
-      {
-        name: "CSS3",
-        icon: <FaCss3Alt className="md:text-4xl text-2xl" color={"#039be5"} />,
-      },
-      {
-        name: "Bootstrap",
-        icon: <FaBootstrap className="md:text-4xl text-2xl" color="#673ab7" />,
-      },
-      {
-        name: "Tailwind CSS",
-        icon: <SiTailwindcss className="md:text-4xl text-2xl" color="#4caf50" />,
-      },
-      {
-        name: "Meterial UI",
-        icon: <SiMui className="md:text-4xl text-2xl" color="#29b6f6" />,
-      },
-      {
-        name: "Chakra UI",
-        icon: <SiChakraui className="md:text-4xl text-2xsl" color="#50cbc0" />
+        name: "AWS",
+        icon: <FaAws className="md:text-4xl text-2xl" color="#000000" />,
       },
       {
         name: "Vercel",
-        icon: <SiVercel className="dark:text-white text-black md:text-4xl text-2xl" />
+        icon: <SiVercel className="md:text-4xl text-2xl" color="#000000" />,
+      },
+      {
+        name: "Networking",
+        icon: <FaNetworkWired className="md:text-4xl text-2xl" color="#fa6700" />,
+      },
+      {
+        name: "Ubuntu",
+        icon: <FaUbuntu className="md:text-4xl text-2xl" color={"#039be5"} />,
       },
       {
         name: "Github",
-        icon: <BsGithub className="md:text-4xl text-2xl" color="#c9d1d9" />
+        icon: <BsGithub className="md:text-4xl text-2xl" color="#000000" />
       },
+      {
+        name: "Server",
+        icon: <FaServer className="md:text-4xl text-2xl" color="#673ab7" />,
+      },
+      {
+        name: "Nginx",
+        icon: <SiNginx className="md:text-4xl text-2xl" color="#4caf50" />,
+      },
+      {
+        name: "Virtual Machine",
+        icon: <SiVirtualbox className="md:text-4xl text-2xl" color="#039be5" />,
+      },
+      {
+        name: "Cloud Computing",
+        icon: <TbCloudComputing className="md:text-4xl text-2xsl" color="#000000" />
+      },
+      {
+        name: "Learning",
+        icon: <SiFuturelearn className="md:text-4xl text-2xsl" color="#000000" />
+      },
+      
     ],
     Good: [
       {
@@ -134,6 +136,10 @@ export const TechStackData = [
       {
         name: "Shopify",
         icon: <FaShopify className="md:text-4xl text-2xl" color="#99c14f" />
+      },
+      {
+        name: "Wordpress",
+        icon: <FaWordpress className="md:text-4xl text-2xl" color="#000000" />
       }
     ]
 
