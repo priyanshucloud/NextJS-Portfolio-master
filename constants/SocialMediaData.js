2fb84aa7be6c0b596b49a19cef7cc6b24572460e
priyanshucloud <admin@priyanshusharma.tech>
2024-08-21 20:52:11 +0530

    Update SocialMediaData.js

--- constants/SocialMediaData.js
+++ constants/SocialMediaData.js
@@ -9,20 +9,24 @@ export const SocialMediaData = [
     color:
       "linear-gradient(135deg, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #FFDC80)",
     link: "https://www.instagram.com/its_13_shaan",
+    'aria-label': "Visit Instagram"
   },
   {
     icon: <IoClose />,
     color: "#0f0f0f",
     link: "https://twitter.com",
-  },
+    'aria-label': "Visit Twitter"
+  },  
   {
     icon: <FaLinkedinIn />,
     color: "#0072b1",
     link: "https://www.linkedin.com/in/",
+    'aria-label': "Visit Linkdin"
   },
   {
     icon: <BsGithub />,
     color: "#171515",
     link: "https://www.github.com/priyanshucloud",
-  },
+    'aria-label': "Visit Github"
+  }
 ];
