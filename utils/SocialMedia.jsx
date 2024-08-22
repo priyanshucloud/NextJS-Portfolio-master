fb741cc669637cf170d1237c5ba3485fae5501b9
priyanshucloud <admin@priyanshusharma.tech>
2024-08-22 23:11:00 +0530

    Update SocialMedia.jsx

--- utils/SocialMedia.jsx
+++ utils/SocialMedia.jsx
@@ -9,9 +9,9 @@ const SocialMedia = () => {
 
   return (
     <Fragment>
-      <div className='absolute left-10 bottom-20 flex flex-col gap-5 z-10'>
-        {SocialMediaData.map((social, key) => (
-          <Link href={social.link} key={key} className='social-media-link'>
+      <div className='social-media-container absolute left-10 bottom-20 flex flex-col gap-5 z-10'>
+        {SocialMediaData.map((social, index) => (
+          <Link href={social.link} key={index} className='social-media-link'>
             <div
               className='social-media-icon p-2 rounded-full text-xl text-white'
               style={{ background: social.color }}
