defbe10ac6f7544da3b0443e230d26d3a339987d
priyanshucloud <admin@priyanshusharma.tech>
2024-08-22 21:23:52 +0530

    Update SocialMediaData.js

--- constants/SocialMediaData.js
+++ constants/SocialMediaData.js
@@ -1,5 +1,5 @@
 import { AiOutlineInstagram } from "react-icons/ai";
-import { BsGithub, BsTwitter } from "react-icons/bs";  // Import Twitter icon
+import { BsGithub, BsTwitter } from "react-icons/bs";
 import { FaLinkedinIn } from "react-icons/fa";
 
 export const SocialMediaData = [
@@ -8,24 +8,24 @@ export const SocialMediaData = [
     color:
       "linear-gradient(135deg, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #FFDC80)",
     link: "https://www.instagram.com/its_13_shaan",
-    'aria-label': "Visit Instagram"
+    'aria-label': "Visit Instagram profile of Priyanshu"
   },
   {
-    icon: <BsTwitter />,  // Correct icon for Twitter
-    color: "#1DA1F2",     // Twitter's official color
+    icon: <BsTwitter />,
+    color: "#1DA1F2",
     link: "https://twitter.com",
-    'aria-label': "Visit Twitter"
+    'aria-label': "Visit Priyanshu's Twitter profile"
   },  
   {
     icon: <FaLinkedinIn />,
     color: "#0072b1",
     link: "https://www.linkedin.com/in/",
-    'aria-label': "Visit LinkedIn"
+    'aria-label': "Visit Priyanshu's LinkedIn profile"
   },
   {
     icon: <BsGithub />,
     color: "#171515",
     link: "https://www.github.com/priyanshucloud",
-    'aria-label': "Visit Github"
+    'aria-label': "Visit Priyanshu's Github profile"
   }
 ];
