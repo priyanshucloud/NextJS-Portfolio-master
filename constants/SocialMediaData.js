5f60a3f0b0e114b17698b38f43120f59e67a1d24
priyanshucloud <admin@priyanshusharma.tech>
2024-08-22 22:53:32 +0530

    Update SocialMediaData.js

--- constants/SocialMediaData.js
+++ constants/SocialMediaData.js
@@ -1,31 +1,35 @@
-import { AiOutlineInstagram } from "react-icons/ai";
-import { BsGithub, BsTwitter } from "react-icons/bs";
-import { FaLinkedinIn } from "react-icons/fa";
-
+import React from 'react';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { BsGithub, BsTwitter } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
 export const SocialMediaData = [
   {
     icon: <AiOutlineInstagram />,
-    color:
-      "linear-gradient(135deg, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #FFDC80)",
+    color: "linear-gradient(135deg, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #FFDC80)",
     link: "https://www.instagram.com/its_13_shaan",
-    'aria-label': "Visit Instagram profile of Priyanshu"
+    ariaLabel: "Instagram Link",
+    className: ("instagram")
   },
   {
     icon: <BsTwitter />,
     color: "#1DA1F2",
     link: "https://twitter.com",
-    'aria-label': "Visit Priyanshu's Twitter profile"
-  },  
+    ariaLabel: "Twitter Link",
+    className: ("Twitter")
+  },
   {
     icon: <FaLinkedinIn />,
     color: "#0072b1",
     link: "https://www.linkedin.com/in/",
-    'aria-label': "Visit Priyanshu's LinkedIn profile"
+    ariaLabel: "LinkedIn Link",
+    className: ("LinkedIn")
   },
   {
     icon: <BsGithub />,
     color: "#171515",
     link: "https://www.github.com/priyanshucloud",
-    'aria-label': "Visit Priyanshu's Github profile"
+    ariaLabel: "GitHub Link",
+    className: ("Github")
   }
 ];
+
